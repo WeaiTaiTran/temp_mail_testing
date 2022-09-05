93ca43e10aa496cd7407ee647e6f55a71e4a8b5e
Tai Tran <tai.tran@weai.vn>
2022-09-05 23:12:47 +0700

    Add Sqlite Library, reqirement, created folder data, handle temp mail

---
+++ sqlite.py
@@ -0,0 +1,81 @@
+import sqlite3
+import os
+import time
+import datetime
+from cryptohash import sha1
+
+
+DATA = "./data"
+
+if not os.path.exists(DATA):
+    os.mkdir(DATA)
+    print(f"Created {DATA}")
+
+profilePath = f"{DATA}/profile_mail.db"
+mainBoxPath = f"{DATA}/mail_box.db"
+testLog = f"{DATA}/test_log.db"
+
+connectSql = sqlite3.connect(profilePath)
+cursor = connectSql.cursor()
+
+try :
+    res = cursor.execute("SELECT * FROM profile")
+except : 
+    cursor.execute("CREATE TABLE profile(id INTEGER PRIMARY KEY, token, email, account, domain, type, date_created)")
+    print("Created Mail Profile")
+    
+connectSqlMailBox = sqlite3.connect(mainBoxPath)
+cursorMailBox = connectSqlMailBox.cursor()
+
+try : 
+    resMailBox = cursorMailBox.execute("SELECT * FROM mail_box")
+except :
+    cursorMailBox.execute("CREATE TABLE mail_box(id, email_token, send_from, subject, date, attachments, body, textBody, htmlBody)")
+    print("Created Mail Box")
+    
+connectSqlTestLog = sqlite3.connect(testLog)
+cursorTestLog = connectSqlTestLog.cursor()
+
+try : 
+    resTestLog = cursorTestLog.execute("SELECT * FROM test_log")
+except :
+    cursorTestLog.execute("CREATE TABLE test_log(id INTEGER PRIMARY KEY, date_created, log)")
+    print("Created Mail Box")
+
+def createMailProfile(profile):
+    dateCreate = createTimeStamp()
+    token = sha1(f"{profile['email']}{profile['type']}{dateCreate}")
+    
+    emailValid = cursor.execute(f"SELECT * FROM profile WHERE email='{profile['email']}'").fetchall()
+    if len(emailValid) > 0:
+        print("Email Valid in Database")
+    else:
+        cursor.execute("INSERT INTO profile(token, email, account, domain, type, date_created) VALUES(?, ?, ?, ?, ?, ?)", (token, profile['email'], profile['account'], profile['domain'], profile['type'], dateCreate))
+        connectSql.commit()
+    res = cursor.execute(f"SELECT * FROM profile WHERE email='{profile['email']}'").fetchall()
+    print(res)
+    
+def createTimeStamp(date = None):
+    if date is None:
+        timeNow = datetime.datetime.now()
+        currentTime = timeNow.strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        currentTime = date
+    timeStamp = time.mktime(datetime.datetime.strptime(currentTime, "%Y-%m-%d %H:%M:%S").timetuple())
+    return timeStamp
+
+def importMailLetter(letter, emailToken):
+    idValid = cursorMailBox.execute(f"SELECT * FROM mail_box WHERE id='{letter['id']}'").fetchall()
+    if len(idValid) > 0:
+        print("Letter Valid In Database")
+    else :
+        cursorMailBox.execute("INSERT INTO mail_box(id, email_token, send_from, subject, date, attachments, body, textBody, htmlBody) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", (letter['id'], emailToken, letter['from'], letter['subject'], createTimeStamp(letter['date']), letter['attachments'], letter['body'], letter['textBody'], letter['htmlBody']))
+        connectSqlMailBox.commit()
+    res = cursorMailBox.execute(f"SELECT * FROM mail_box WHERE id={letter['id']}").fetchall()
+    print(res)
+    
+# TODO : Find Solution To Test Database
+
+#* TYPE 1 : Created Mail - Invite ClassRoom BY Email
+#* TYPE 2 : Created Mail - Invite ORG BY Email
+#* TYPE 3 : Affter TYPE 1 and TYPE 2 Done
--- temp_mail.py
+++ temp_mail.py
@@ -1,18 +1,43 @@
 import requests
+import re
+import json
 
 QUANLITY_CLASSROOM = 400
 QUANLITY_ORG = 200
 QUANLITY_ORG_CLASSROOM = 200
 
-url = "https://www.1secmail.com/api/v1/"
-email = "1zzd9jyudu8g@qiott.com"
-user = "1zzd9jyudu8g"
-domain = "qiott.com"
-mail_id = "5664249"
-created_account = "?action=genRandomMailbox&count=200"
-login_account = f"?action=getMessages&login={user}&domain={domain}"
-check_mail = f"?action=readMessage&login={user}&domain={domain}&id={mail_id}"
-api = f"{url}{check_mail}"
-response = requests.get(url=api)
+# user = "1zzd9jyudu8g"
+# domain = "qiott.com"
+# mail_id = "5664249"
+# login_account = f"?action=getMessages&login={user}&domain={domain}"
+# check_mail = f"?action=readMessage&login={user}&domain={domain}&id={mail_id}"
+# # response = requests.get(url=api)
+url = "https://www.1secmail.com/api/v1"
 
-print(response.json())
+def create_mail(amount):
+    created_account = "?action=genRandomMailbox&count={amount}"
+    api = f"{url}/{created_account}"
+    response = requests.get(url=api)
+    return response.json()
+
+def excute_mail(mail):
+    account = re.search(r'(\w.+)@', mail).group(1)
+    domain = re.search(r'@(\w.+)', mail).group(1)
+    return account, domain
+
+def get_mail_box_id(account, domain):
+    login_account = f"?action=getMessages&login={account}&domain={domain}"
+    api = f"{url}/{login_account}"
+    response = requests.get(url=api)
+    return response.json()
+
+def access_mail_box(account, domain, mail_box):
+    mail_box = f"?action=readMessage&login={account}&domain={domain}&id={mail_box}"
+    api = f"{url}/{mail_box}"
+    response = requests.get(url=api)
+    return response.json()
+
+
+# print(get_mail_box_id("1zzd9jyudu8g", "qiott.com"))
+# print(json.dumps(access_mail_box("1zzd9jyudu8g", "qiott.com", 5817139)))
+# TODO : Solution - Create Mail -> Excute Mail -> Insert Database -> Call API Add Mail Eduplax Staging -> Get Mail Box ID -> Access Mail Box -> Insert Letter To Database -> Query Database Check Subject -> Query Database Check HTMLbody
